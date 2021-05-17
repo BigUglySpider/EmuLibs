0c50bdf31a255be62fa018fd391b37678294f5c9
BigUglySpider <36843643+BigUglySpider@users.noreply.github.com>
2021-05-17 21:13:56 +0100

    Changes to semantics in emu_vector_get_element

--- EmuMath/EmuMath/EmuMath/_do_not_manually_include/EmuVectorTMPHelpers.h
+++ EmuMath/EmuMath/EmuMath/_do_not_manually_include/EmuVectorTMPHelpers.h
@@ -103,6 +103,7 @@ namespace EmuMath
 		template<class EmuVec__>
 		struct emu_vector_get_element
 		{
+		private:
 			using _non_qualified_vec = std::remove_cv_t<EmuVec__>;
 			static constexpr bool _is_emu_vector = EmuMath::TMPHelpers::is_emu_vector_v<_non_qualified_vec>;
 			static_assert
@@ -110,8 +111,7 @@ namespace EmuMath
 				_is_emu_vector,
 				"Failed to retrieve an element from an EmuMath::Vector type as the passed type was not an EmuMath::Vector."
 			);
-			using _temp_value_type = typename _non_qualified_vec::value_type;
-			using _value_type = std::conditional_t<_is_emu_vector, _temp_value_type, int>;
+			using _value_type = std::conditional_t<_is_emu_vector, typename _non_qualified_vec::value_type, int>;
 
 			static constexpr bool _has_z = _non_qualified_vec::size() >= 3;
 			static constexpr bool _has_w = _non_qualified_vec::size() >= 4;
@@ -120,33 +120,34 @@ namespace EmuMath
 			using _const_ref_value_type = typename EmuMath::_info::VectorInfo<_value_type>::const_ref_value_type;
 			using _nonref_value_type = typename EmuMath::_info::VectorInfo<_value_type>::nonref_value_type;
 
-			using _out_x = _ref_value_type;
-			using _out_y = _ref_value_type;
-			using _out_z = std::conditional_t<_has_z, _ref_value_type, _nonref_value_type>;
-			using _out_w = std::conditional_t<_has_w, _ref_value_type, _nonref_value_type>;
+		public:
+			using out_x = _ref_value_type;
+			using out_y = _ref_value_type;
+			using out_z = std::conditional_t<_has_z, _ref_value_type, _nonref_value_type>;
+			using out_w = std::conditional_t<_has_w, _ref_value_type, _nonref_value_type>;
 
-			using _out_const_x = _const_ref_value_type;
-			using _out_const_y = _const_ref_value_type;
-			using _out_const_z = std::conditional_t<_has_z, _const_ref_value_type, _nonref_value_type>;
-			using _out_const_w = std::conditional_t<_has_w, _const_ref_value_type, _nonref_value_type>;
+			using out_const_x = _const_ref_value_type;
+			using out_const_y = _const_ref_value_type;
+			using out_const_z = std::conditional_t<_has_z, _const_ref_value_type, _nonref_value_type>;
+			using out_const_w = std::conditional_t<_has_w, _const_ref_value_type, _nonref_value_type>;
 
-			static constexpr _out_x X(_non_qualified_vec& vec_)
+			static constexpr out_x X(_non_qualified_vec& vec_)
 			{
 				return vec_.x;
 			}
-			static constexpr _out_const_x X(const _non_qualified_vec& vec_)
+			static constexpr out_const_x X(const _non_qualified_vec& vec_)
 			{
 				return vec_.x;
 			}
-			static constexpr _out_y Y(_non_qualified_vec& vec_)
+			static constexpr out_y Y(_non_qualified_vec& vec_)
 			{
 				return vec_.y;
 			}
-			static constexpr _out_const_y Y(const _non_qualified_vec& vec_)
+			static constexpr out_const_y Y(const _non_qualified_vec& vec_)
 			{
 				return vec_.y;
 			}
-			static constexpr _out_z Z(_non_qualified_vec& vec_)
+			static constexpr out_z Z(_non_qualified_vec& vec_)
 			{
 				if constexpr (_has_z)
 				{
@@ -154,10 +155,10 @@ namespace EmuMath
 				}
 				else
 				{
-					return EmuCore::ArithmeticHelpers::ZeroT<_nonref_value_type>;
+					return out_const_z();
 				}
 			}
-			static constexpr _out_const_z Z(const _non_qualified_vec& vec_)
+			static constexpr out_const_z Z(const _non_qualified_vec& vec_)
 			{
 				if constexpr (_has_z)
 				{
@@ -165,10 +166,10 @@ namespace EmuMath
 				}
 				else
 				{
-					return EmuCore::ArithmeticHelpers::ZeroT<_nonref_value_type>;
+					return out_const_z();
 				}
 			}
-			static constexpr _out_w W(_non_qualified_vec& vec_)
+			static constexpr out_w W(_non_qualified_vec& vec_)
 			{
 				if constexpr (_has_w)
 				{
@@ -176,10 +177,10 @@ namespace EmuMath
 				}
 				else
 				{
-					return EmuCore::ArithmeticHelpers::ZeroT<_nonref_value_type>;
+					return out_const_w();
 				}
 			}
-			static constexpr _out_const_w W(const _non_qualified_vec& vec_)
+			static constexpr out_const_w W(const _non_qualified_vec& vec_)
 			{
 				if constexpr (_has_w)
 				{
@@ -187,7 +188,7 @@ namespace EmuMath
 				}
 				else
 				{
-					return EmuCore::ArithmeticHelpers::ZeroT<_nonref_value_type>;
+					return out_const_w();
 				}
 			}
 		};
@@ -196,7 +197,7 @@ namespace EmuMath
 		/// <param name="vec_">EmuMath Vector to retrieve the x element of.</param>
 		/// <returns>Reference to the passed Vector's x element.</returns>
 		template<typename EmuVec_>
-		static constexpr typename emu_vector_get_element<EmuVec_>::_out_x emu_vector_x(EmuVec_& vec_)
+		static constexpr typename emu_vector_get_element<EmuVec_>::out_x emu_vector_x(EmuVec_& vec_)
 		{
 			return emu_vector_get_element<EmuVec_>::X(vec_);
 		}
@@ -205,7 +206,7 @@ namespace EmuMath
 		/// <param name="vec_">EmuMath Vector to retrieve the x element of.</param>
 		/// <returns>Constant reference to the passed Vector's x element.</returns>
 		template<typename EmuVec_>
-		static constexpr typename emu_vector_get_element<EmuVec_>::_out_const_x emu_vector_x(const EmuVec_& vec_)
+		static constexpr typename emu_vector_get_element<EmuVec_>::out_const_x emu_vector_x(const EmuVec_& vec_)
 		{
 			return emu_vector_get_element<EmuVec_>::X(vec_);
 		}
@@ -214,7 +215,7 @@ namespace EmuMath
 		/// <param name="vec_">EmuMath Vector to retrieve the y element of.</param>
 		/// <returns>Reference to the passed Vector's y element.</returns>
 		template<typename EmuVec_>
-		static constexpr typename emu_vector_get_element<EmuVec_>::_out_y emu_vector_y(EmuVec_& vec_)
+		static constexpr typename emu_vector_get_element<EmuVec_>::out_y emu_vector_y(EmuVec_& vec_)
 		{
 			return emu_vector_get_element<EmuVec_>::Y(vec_);
 		}
@@ -223,51 +224,51 @@ namespace EmuMath
 		/// <param name="vec_">EmuMath Vector to retrieve the y element of.</param>
 		/// <returns>Constant reference to the passed Vector's y element.</returns>
 		template<typename EmuVec_>
-		static constexpr typename emu_vector_get_element<EmuVec_>::_out_const_y emu_vector_y(const EmuVec_& vec_)
+		static constexpr typename emu_vector_get_element<EmuVec_>::out_const_y emu_vector_y(const EmuVec_& vec_)
 		{
 			return emu_vector_get_element<EmuVec_>::Y(vec_);
 		}
 		/// <summary>
-		/// Returns a reference to the passed Vector's z element if it has one, or a copy of a zero-initialised item of the Vector's contained type if it does not have one.
+		/// Returns a reference to the passed Vector's z element if it has one, or a default constructed item of the Vector's contained type if it does not have one.
 		/// </summary>
 		/// <typeparam name="EmuVec_">Type of EmuMath Vector to retrieve the z element of.</typeparam>
 		/// <param name="vec_">EmuMath Vector to retrieve the z element of.</param>
 		/// <returns>Reference to the passed Vector's z element, or a zero-initialised item of the Vector's contained type if it does not have one.</returns>
 		template<typename EmuVec_>
-		static constexpr typename emu_vector_get_element<EmuVec_>::_out_z emu_vector_z(EmuVec_& vec_)
+		static constexpr typename emu_vector_get_element<EmuVec_>::out_z emu_vector_z(EmuVec_& vec_)
 		{
 			return emu_vector_get_element<EmuVec_>::Z(vec_);
 		}
 		/// <summary>
-		/// Returns a constant reference to the passed Vector's z element if it has one, or a copy of a zero-initialised item of the Vector's contained type if it does not have one.
+		/// Returns a constant reference to the passed Vector's z element if it has one, or a default constructed item of the Vector's contained type if it does not have one.
 		/// </summary>
 		/// <typeparam name="EmuVec_">Type of EmuMath Vector to retrieve the z element of.</typeparam>
 		/// <param name="vec_">EmuMath Vector to retrieve the z element of.</param>
 		/// <returns>Constant reference to the passed Vector's z element, or a zero-initialised item of the Vector's contained type if it does not have one.</returns>
 		template<typename EmuVec_>
-		static constexpr typename emu_vector_get_element<EmuVec_>::_out_const_z emu_vector_z(const EmuVec_& vec_)
+		static constexpr typename emu_vector_get_element<EmuVec_>::out_const_z emu_vector_z(const EmuVec_& vec_)
 		{
 			return emu_vector_get_element<EmuVec_>::Z(vec_);
 		}
 		/// <summary>
-		/// Returns a reference to the passed Vector's w element if it has one, or a copy of a zero-initialised item of the Vector's contained type if it does not have one.
+		/// Returns a reference to the passed Vector's w element if it has one, or a default constructed item of the Vector's contained type if it does not have one.
 		/// </summary>
 		/// <typeparam name="EmuVec_">Type of EmuMath Vector to retrieve the w element of.</typeparam>
 		/// <param name="vec_">EmuMath Vector to retrieve the w element of.</param>
 		/// <returns>Reference to the passed Vector's w element, or a zero-initialised item of the Vector's contained type if it does not have one.</returns>
 		template<typename EmuVec_>
-		static constexpr typename emu_vector_get_element<EmuVec_>::_out_w emu_vector_w(EmuVec_& vec_)
+		static constexpr typename emu_vector_get_element<EmuVec_>::out_w emu_vector_w(EmuVec_& vec_)
 		{
 			return emu_vector_get_element<EmuVec_>::W(vec_);
 		}
 		/// <summary>
-		/// Returns a constant reference to the passed Vector's w element if it has one, or a copy of a zero-initialised item of the Vector's contained type if it does not have one.
+		/// Returns a constant reference to the passed Vector's w element if it has one, or a default constructed item of the Vector's contained type if it does not have one.
 		/// </summary>
 		/// <typeparam name="EmuVec_">Type of EmuMath Vector to retrieve the w element of.</typeparam>
 		/// <param name="vec_">EmuMath Vector to retrieve the w element of.</param>
 		/// <returns>Constant reference to the passed Vector's w element, or a zero-initialised item of the Vector's contained type if it does not have one.</returns>
 		template<typename EmuVec_>
-		static constexpr typename emu_vector_get_element<EmuVec_>::_out_const_w emu_vector_w(const EmuVec_& vec_)
+		static constexpr typename emu_vector_get_element<EmuVec_>::out_const_w emu_vector_w(const EmuVec_& vec_)
 		{
 			return emu_vector_get_element<EmuVec_>::W(vec_);
 		}
