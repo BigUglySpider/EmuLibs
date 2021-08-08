c40943a879a95243d76884e90f9f35d089842d99
BigUglySpider <36843643+BigUglySpider@users.noreply.github.com>
2021-08-08 01:59:59 +0100

    fast vector inlines

--- EmuMath/EmuMath/EmuMath/_do_not_manually_include/FastVectorf4.h
+++ EmuMath/EmuMath/EmuMath/_do_not_manually_include/FastVectorf4.h
@@ -18,30 +18,30 @@ namespace EmuMath
 #pragma endregion
 
 #pragma region CONSTRUCTORS
-		FastVector() : FastVector(0.0f, 0.0f, 0.0f, 0.0f)
+		inline FastVector() : FastVector(0.0f, 0.0f, 0.0f, 0.0f)
 		{
 		}
-		FastVector(const this_type& toCopy_) : vectorData(toCopy_.vectorData)
+		inline FastVector(const this_type& toCopy_) : vectorData(toCopy_.vectorData)
 		{
 		}
-		explicit FastVector(__m128 toCopy_) : vectorData(toCopy_)
+		explicit inline FastVector(__m128 toCopy_) : vectorData(toCopy_)
 		{
 		}
 		template<typename X_, typename Y_, typename Z_, typename W_>
-		explicit FastVector(const X_& x_, const Y_& y_, const Z_& z_, const W_& w_) :
+		explicit inline FastVector(const X_& x_, const Y_& y_, const Z_& z_, const W_& w_) :
 			vectorData(_mm_setr_ps(static_cast<value_type>(x_), static_cast<value_type>(y_), static_cast<value_type>(z_), static_cast<value_type>(w_)))
 		{
 		}
 		template<typename ToCopyT>
-		explicit FastVector(const EmuMath::Vector2<ToCopyT>& toCopy_) : FastVector(toCopy_.x, toCopy_.y, 0.0f, 0.0f)
+		explicit inline FastVector(const EmuMath::Vector2<ToCopyT>& toCopy_) : FastVector(toCopy_.x, toCopy_.y, 0.0f, 0.0f)
 		{
 		}
 		template<typename ToCopyT>
-		explicit FastVector(const EmuMath::Vector3<ToCopyT>& toCopy_) : FastVector(toCopy_.x, toCopy_.y, toCopy_.z, 0.0f)
+		explicit inline FastVector(const EmuMath::Vector3<ToCopyT>& toCopy_) : FastVector(toCopy_.x, toCopy_.y, toCopy_.z, 0.0f)
 		{
 		}
 		template<typename ToCopyT>
-		explicit FastVector(const EmuMath::Vector4<ToCopyT>& toCopy_) : FastVector(toCopy_.x, toCopy_.y, toCopy_.z, toCopy_.w)
+		explicit inline FastVector(const EmuMath::Vector4<ToCopyT>& toCopy_) : FastVector(toCopy_.x, toCopy_.y, toCopy_.z, toCopy_.w)
 		{
 		}
 #pragma endregion
@@ -50,7 +50,7 @@ namespace EmuMath
 		/// <summary> Retrieves a copy of the data at the given index of this Vector. The at function cannot be used to obtain a reference. </summary>
 		/// <returns>Copy of the value at the provided index.</returns>
 		template<std::size_t Index_>
-		float at() const
+		inline float at() const
 		{
 			return EmuMath::SIMD::get_m128_index<Index_>(vectorData);
 		}
@@ -59,130 +59,130 @@ namespace EmuMath
 		/// <para> If the desired index_ is compile-time evaluable, it is recommended to provide it as a template argument instead of a function argument. </para>
 		/// </summary>
 		/// <returns>Copy of the value at the provided index.</returns>
-		float at(std::size_t index_) const
+		inline float at(std::size_t index_) const
 		{
 			return EmuMath::SIMD::get_m128_index(vectorData, index_);
 		}
-		float operator[](const std::size_t index_) const
+		inline float operator[](const std::size_t index_) const
 		{
 			return this->at(index_);
 		}
 #pragma endregion
 
 #pragma region ARITHMETIC_CONST_OPERATORS
-		this_type operator+(const this_type& rhs) const
+		inline this_type operator+(const this_type& rhs) const
 		{
 			return this->operator+(rhs.vectorData);
 		}
-		this_type operator+(const __m128 rhs) const
+		inline this_type operator+(const __m128 rhs) const
 		{
 			return this_type(_mm_add_ps(vectorData, rhs));
 		}
-		this_type operator-(const this_type& rhs) const
+		inline this_type operator-(const this_type& rhs) const
 		{
 			return this->operator-(rhs.vectorData);
 		}
-		this_type operator-(const __m128 rhs) const
+		inline this_type operator-(const __m128 rhs) const
 		{
 			return this_type(_mm_sub_ps(vectorData, rhs));
 		}
-		this_type operator*(const this_type& rhs) const
+		inline this_type operator*(const this_type& rhs) const
 		{
 			return this->operator*(rhs.vectorData);
 		}
-		this_type operator*(const float rhs) const
+		inline this_type operator*(const float rhs) const
 		{
 			return this->operator*(_mm_set_ps1(rhs));
 		}
-		this_type operator*(const __m128 rhs) const
+		inline this_type operator*(const __m128 rhs) const
 		{
 			return this_type(_mm_mul_ps(vectorData, rhs));
 		}
-		this_type operator/(const this_type& rhs) const
+		inline this_type operator/(const this_type& rhs) const
 		{
 			return this->operator/(rhs.vectorData);
 		}
-		this_type operator/(const float rhs) const
+		inline this_type operator/(const float rhs) const
 		{
 			return this->operator/(_mm_set_ps1(rhs));
 		}
-		this_type operator/(const __m128 rhs) const
+		inline this_type operator/(const __m128 rhs) const
 		{
 			return this_type(_mm_div_ps(vectorData, rhs));
 		}
 #pragma endregion
 
 #pragma region BITWISE_CONST_OPERATORS
-		this_type operator&(const this_type& rhs) const
+		inline this_type operator&(const this_type& rhs) const
 		{
 			return this->operator&(rhs.vectorData);
 		}
-		this_type operator&(const __m128 rhs) const
+		inline this_type operator&(const __m128 rhs) const
 		{
 			return this_type(_mm_and_ps(vectorData, rhs));
 		}
-		this_type operator|(const this_type& rhs) const
+		inline this_type operator|(const this_type& rhs) const
 		{
 			return this->operator|(rhs.vectorData);
 		}
-		this_type operator|(const __m128 rhs) const
+		inline this_type operator|(const __m128 rhs) const
 		{
 			return this_type(_mm_or_ps(vectorData, rhs));
 		}
-		this_type operator^(const this_type& rhs) const
+		inline this_type operator^(const this_type& rhs) const
 		{
 			return this->operator^(rhs.vectorData);
 		}
-		this_type operator^(const __m128 rhs) const
+		inline this_type operator^(const __m128 rhs) const
 		{
 			return this_type(_mm_xor_ps(vectorData, rhs));
 		}
 #pragma endregion
 
 #pragma region ARITHMETIC_NONCONST_OPERATORS
-		this_type& operator+=(const this_type& rhs)
+		inline this_type& operator+=(const this_type& rhs)
 		{
 			return this->operator+=(rhs.vectorData);
 		}
-		this_type& operator+=(const __m128 rhs)
+		inline this_type& operator+=(const __m128 rhs)
 		{
 			vectorData = _mm_add_ps(vectorData, rhs);
 			return *this;
 		}
 
-		this_type& operator-=(const this_type& rhs)
+		inline this_type& operator-=(const this_type& rhs)
 		{
 			return this->operator-=(rhs.vectorData);
 		}
-		this_type& operator-=(const __m128 rhs)
+		inline this_type& operator-=(const __m128 rhs)
 		{
 			vectorData = _mm_sub_ps(vectorData, rhs);
 			return *this;
 		}
 
-		this_type& operator*=(const this_type& rhs)
+		inline this_type& operator*=(const this_type& rhs)
 		{
 			return this->operator*=(rhs.vectorData);
 		}
-		this_type& operator*=(const float rhs)
+		inline this_type& operator*=(const float rhs)
 		{
 			return this->operator*=(_mm_set_ps1(rhs));
 		}
-		this_type& operator*=(const __m128 rhs)
+		inline this_type& operator*=(const __m128 rhs)
 		{
 			vectorData = _mm_mul_ps(vectorData, rhs);
 			return *this;
 		}
 
-		this_type& operator/=(const this_type& rhs)
+		inline this_type& operator/=(const this_type& rhs)
 		{
 			return this->operator/=(rhs.vectorData);
 		}
-		this_type& operator/=(const float rhs)
+		inline this_type& operator/=(const float rhs)
 		{
 			return this->operator/=(_mm_set_ps1(rhs));
 		}
-		this_type& operator/=(const __m128 rhs)
+		inline this_type& operator/=(const __m128 rhs)
 		{
 			vectorData = _mm_div_ps(vectorData, rhs);
 			return *this;
@@ -190,31 +190,31 @@ namespace EmuMath
 #pragma endregion
 
 #pragma region BITWISE_NONCONST_OPERATORS
-		this_type& operator&=(const this_type& rhs)
+		inline this_type& operator&=(const this_type& rhs)
 		{
 			return this->operator&=(rhs.vectorData);
 		}
-		this_type& operator&=(const __m128 rhs)
+		inline this_type& operator&=(const __m128 rhs)
 		{
 			vectorData = _mm_and_ps(vectorData, rhs);
 			return *this;
 		}
 
-		this_type& operator|=(const this_type& rhs)
+		inline this_type& operator|=(const this_type& rhs)
 		{
 			return this->operator|=(rhs.vectorData);
 		}
-		this_type& operator|=(const __m128 rhs)
+		inline this_type& operator|=(const __m128 rhs)
 		{
 			vectorData = _mm_or_ps(vectorData, rhs);
 			return *this;
 		}
 
-		this_type& operator^=(const this_type& rhs)
+		inline this_type& operator^=(const this_type& rhs)
 		{
 			return this->operator^=(rhs.vectorData);
 		}
-		this_type& operator^=(const __m128 rhs)
+		inline this_type& operator^=(const __m128 rhs)
 		{
 			vectorData = _mm_xor_ps(vectorData, rhs);
 			return *this;
@@ -222,42 +222,42 @@ namespace EmuMath
 #pragma endregion
 
 #pragma region BOOLEAN_COMPARISON_OPERATORS
-		bool operator==(const this_type& rhs) const
+		inline bool operator==(const this_type& rhs) const
 		{
 			return this->operator==(rhs.vectorData);
 		}
-		bool operator==(const __m128 rhs) const
+		inline bool operator==(const __m128 rhs) const
 		{
 			return EmuMath::SIMD::all_equal<true, true, true, true>(vectorData, rhs);
 		}
-		bool operator!=(const this_type& rhs) const
+		inline bool operator!=(const this_type& rhs) const
 		{
 			return this->operator!=(rhs.vectorData);
 		}
-		bool operator!=(const __m128 rhs) const
+		inline bool operator!=(const __m128 rhs) const
 		{
 			return !EmuMath::SIMD::all_equal<true, true, true, true>(vectorData, rhs);
 		}
-		bool operator>(const this_type& rhs) const
+		inline bool operator>(const this_type& rhs) const
 		{
 			return this->SquareMagnitude() > rhs.SquareMagnitude();
 		}
-		bool operator<(const this_type& rhs) const
+		inline bool operator<(const this_type& rhs) const
 		{
 			return this->SquareMagnitude() < rhs.SquareMagnitude();
 		}
-		bool operator>=(const this_type& rhs) const
+		inline bool operator>=(const this_type& rhs) const
 		{
 			return this->SquareMagnitude() >= rhs.SquareMagnitude();
 		}
-		bool operator<=(const this_type& rhs) const
+		inline bool operator<=(const this_type& rhs) const
 		{
 			return this->SquareMagnitude() <= rhs.SquareMagnitude();
 		}
 #pragma endregion
 
 #pragma region ASSIGNMENT_OPERATORS
-		this_type& operator=(const this_type& toCopy_)
+		inline this_type& operator=(const this_type& toCopy_)
 		{
 			vectorData = toCopy_.vectorData;
 			return *this;
@@ -266,7 +266,7 @@ namespace EmuMath
 
 #pragma region SHUFFLES
 		template<std::size_t X_, std::size_t Y_, std::size_t Z_, std::size_t W_>
-		this_type Shuffle() const
+		inline this_type Shuffle() const
 		{
 			return this_type(EmuMath::SIMD::shuffle<X_, Y_, Z_, W_>(vectorData));
 		}
@@ -274,13 +274,13 @@ namespace EmuMath
 
 #pragma region SETS
 		template<class Rhs_>
-		this_type& Set(const Rhs_ toSetAllTo_)
+		inline this_type& Set(const Rhs_ toSetAllTo_)
 		{
 			vectorData = _mm_set_ps1(static_cast<float>(toSetAllTo_));
 			return *this;
 		}
 		template<class X_, class Y_, class Z_, class W_>
-		this_type& Set(const X_ x_, const Y_ y_, const Z_ z_, const W_ w_)
+		inline this_type& Set(const X_ x_, const Y_ y_, const Z_ z_, const W_ w_)
 		{
 			vectorData = _mm_set_ps(static_cast<float>(w_), static_cast<float>(z_), static_cast<float>(y_), static_cast<float>(x_));
 			return *this;
@@ -288,31 +288,31 @@ namespace EmuMath
 #pragma endregion
 
 #pragma region OVERALL_OPERATIONS
-		this_type Reciprocal() const
+		inline this_type Reciprocal() const
 		{
 			return this_type(_mm_div_ps(_mm_set_ps1(1.0f), vectorData));
 		}
 
-		float TotalSum() const
+		inline float TotalSum() const
 		{
 			return EmuMath::SIMD::horizontal_vector_sum_scalar(vectorData);
 		}
 
-		float Min() const
+		inline float Min() const
 		{
 			__m128 result = _mm_min_ps(vectorData, EmuMath::SIMD::shuffle<3, 0, 1, 2>(vectorData));
 			result = _mm_min_ps(result, EmuMath::SIMD::shuffle<2, 3, 1, 0>(result));
 			result = _mm_min_ps(result, EmuMath::SIMD::shuffle<1, 2, 3, 0>(result));
 			return _mm_cvtss_f32(result);
 		}
-		float Max() const
+		inline float Max() const
 		{
 			__m128 result = _mm_max_ps(vectorData, EmuMath::SIMD::shuffle<3, 0, 1, 2>(vectorData));
 			result = _mm_max_ps(result, EmuMath::SIMD::shuffle<2, 3, 1, 0>(result));
 			result = _mm_max_ps(result, EmuMath::SIMD::shuffle<1, 2, 3, 0>(result));
 			return _mm_cvtss_f32(result);
 		}
-		float Mean() const
+		inline float Mean() const
 		{
 			// No advantage from SIMD due to the required horizontal addition
 			float scalarData[4];
@@ -320,69 +320,69 @@ namespace EmuMath
 			return (scalarData[0] + scalarData[1] + scalarData[2] + scalarData[3]) * 0.25f;
 		}
 
-		this_type ClampMin(const this_type min_) const
+		inline this_type ClampMin(const this_type min_) const
 		{
 			return this->ClampMin(min_.vectorData);
 		}
-		this_type ClampMin(const float min_) const
+		inline this_type ClampMin(const float min_) const
 		{
 			return this->ClampMin(_mm_set_ps1(min_));
 		}
-		this_type ClampMin(const __m128 min_) const
+		inline this_type ClampMin(const __m128 min_) const
 		{
 			__m128 ignoreFlags = _mm_cmpge_ps(vectorData, min_);
 			__m128 result = _mm_and_ps(ignoreFlags, vectorData);
 			result = _mm_or_ps(result, _mm_andnot_ps(ignoreFlags, min_));
 			return this_type(result);
 		}
-		this_type ClampMax(const this_type max_) const
+		inline this_type ClampMax(const this_type max_) const
 		{
 			return this->ClampMax(max_.vectorData);
 		}
-		this_type ClampMax(const float max_) const
+		inline this_type ClampMax(const float max_) const
 		{
 			return this->ClampMax(_mm_set_ps1(max_));
 		}
-		this_type ClampMax(__m128 max_) const
+		inline this_type ClampMax(__m128 max_) const
 		{
 			__m128 ignoreFlags = _mm_cmple_ps(vectorData, max_);
 			__m128 result = _mm_and_ps(ignoreFlags, vectorData);
 			result = _mm_or_ps(result, _mm_andnot_ps(ignoreFlags, max_));
 			return this_type(result);
 		}
-		this_type Clamp(const this_type min_, const this_type max_) const
+		inline this_type Clamp(const this_type min_, const this_type max_) const
 		{
 			return this->Clamp(min_.vectorData, max_.vectorData);
 		}
-		this_type Clamp(const this_type min_, const __m128 max_) const
+		inline this_type Clamp(const this_type min_, const __m128 max_) const
 		{
 			return this->Clamp(min_.vectorData, max_);
 		}
-		this_type Clamp(const __m128 min_, const this_type max_) const
+		inline this_type Clamp(const __m128 min_, const this_type max_) const
 		{
 			return this->Clamp(min_, max_.vectorData);
 		}
-		this_type Clamp(const this_type min_, const float max_) const
+		inline this_type Clamp(const this_type min_, const float max_) const
 		{
 			return this->Clamp(min_.vectorData, _mm_set_ps1(max_));
 		}
-		this_type Clamp(const float min_, const this_type max_) const
+		inline this_type Clamp(const float min_, const this_type max_) const
 		{
 			return this->Clamp(_mm_set_ps1(min_), max_.vectorData);
 		}
-		this_type Clamp(const float min_, const __m128 max_) const
+		inline this_type Clamp(const float min_, const __m128 max_) const
 		{
 			return this->Clamp(_mm_set_ps1(min_), max_);
 		}
-		this_type Clamp(const __m128 min_, const float max_) const
+		inline this_type Clamp(const __m128 min_, const float max_) const
 		{
 			return this->Clamp(min_, _mm_set_ps1(max_));
 		}
-		this_type Clamp(const float min_, const float max_) const
+		inline this_type Clamp(const float min_, const float max_) const
 		{
 			return this->Clamp(_mm_set_ps1(min_), _mm_set_ps1(max_));
 		}
-		this_type Clamp(const __m128 min_, const __m128 max_) const
+		inline this_type Clamp(const __m128 min_, const __m128 max_) const
 		{
 			__m128 clampMinFlags = _mm_cmplt_ps(vectorData, min_);
 			__m128 clampMaxFlags = _mm_cmpgt_ps(vectorData, max_);
@@ -394,40 +394,40 @@ namespace EmuMath
 			return this_type(result);
 		}
 
-		this_type Floor() const
+		inline this_type Floor() const
 		{
 			return this_type(_mm_round_ps(vectorData, _MM_FROUND_FLOOR));
 		}
-		this_type Ceil() const
+		inline this_type Ceil() const
 		{
 			return this_type(_mm_round_ps(vectorData, _MM_FROUND_CEIL));
 		}
-		this_type Trunc() const
+		inline this_type Trunc() const
 		{
 			return this_type(_mm_round_ps(vectorData, _MM_FROUND_TRUNC));
 		}
 
-		this_type Lerp(const this_type& b_, const float t_)
+		inline this_type Lerp(const this_type& b_, const float t_)
 		{
 			return this->Lerp(b_.vectorData, _mm_set_ps1(t_));
 		}
-		this_type Lerp(const this_type& b_, const this_type& t_)
+		inline this_type Lerp(const this_type& b_, const this_type& t_)
 		{
 			return this->Lerp(b_.vectorData, t_.vectorData);
 		}
-		this_type Lerp(const this_type& b_, const __m128 t_)
+		inline this_type Lerp(const this_type& b_, const __m128 t_)
 		{
 			return this->Lerp(b_.vectorData, t_);
 		}
-		this_type Lerp(const __m128 b_, const float t_)
+		inline this_type Lerp(const __m128 b_, const float t_)
 		{
 			return this->Lerp(b_, _mm_set_ps1(t_));
 		}
-		this_type Lerp(const __m128 b_, const this_type& t_)
+		inline this_type Lerp(const __m128 b_, const this_type& t_)
 		{
 			return this->Lerp(b_, t_.vectorData);
 		}
-		this_type Lerp(const __m128 b_, const __m128 t_)
+		inline this_type Lerp(const __m128 b_, const __m128 t_)
 		{
 			// Follows approximation of "lerped = a + (b - a) * t"
 			__m128 result = _mm_sub_ps(b_, vectorData);
@@ -438,30 +438,30 @@ namespace EmuMath
 #pragma endregion
 
 #pragma region VECTOR_OPERATIONS
-		float SquareMagnitude() const
+		inline float SquareMagnitude() const
 		{
 			return _calculate_square_mag_128().m128_f32[0];
 		}
-		float Magnitude() const
+		inline float Magnitude() const
 		{
 			return _calculate_mag_128().m128_f32[0];
 		}
 
-		this_type Normalise() const
+		inline this_type Normalise() const
 		{
 			return this_type(_mm_div_ps(vectorData, _calculate_mag_128()));
 		}
 
-		float DotProduct(const this_type& b_) const
+		inline float DotProduct(const this_type& b_) const
 		{
 			return this->DotProduct(b_.vectorData);
 		}
-		float DotProduct(const __m128 b_) const
+		inline float DotProduct(const __m128 b_) const
 		{
 			return _mm_dp_ps(vectorData, b_, 0xFF).m128_f32[0];
 		}
 
-		this_type Reverse() const
+		inline this_type Reverse() const
 		{
 			return this_type(_mm_mul_ps(vectorData, _mm_set_ps1(-1.0f)));
 		}
@@ -469,7 +469,7 @@ namespace EmuMath
 
 #pragma region CONVERSION_FUNCTIONS
 		template<std::size_t OutSize_ = size, typename OutT_ = value_type>
-		typename EmuMath::TMPHelpers::emu_vector_from_size<OutSize_, OutT_>::type AsEmuVector() const
+		inline typename EmuMath::TMPHelpers::emu_vector_from_size<OutSize_, OutT_>::type AsEmuVector() const
 		{
 			float scalarData[4];
 			_mm_store_ps(scalarData, vectorData);
@@ -496,13 +496,13 @@ namespace EmuMath
 		__m128 vectorData;
 
 	private:
-		__m128 _calculate_square_mag_128() const
+		inline __m128 _calculate_square_mag_128() const
 		{
 			__m128 result = _mm_mul_ps(vectorData, vectorData);
 			return EmuMath::SIMD::horizontal_vector_sum_fill(result);
 		}
 
-		__m128 _calculate_mag_128() const
+		inline __m128 _calculate_mag_128() const
 		{
 			return _mm_sqrt_ps(_calculate_square_mag_128());
 		}
