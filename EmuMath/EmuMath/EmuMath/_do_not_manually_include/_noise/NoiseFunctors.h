811e4707ba6ab285c8973fc7ad5211f89573d5e2
BigUglySpider <36843643+BigUglySpider@users.noreply.github.com>
2021-10-14 19:56:53 +0100

    Blanket NoiseFunctor file for future proofing includes

---
+++ EmuMath/EmuMath/EmuMath/_do_not_manually_include/_noise/NoiseFunctors.h
@@ -0,0 +1,7 @@
+#ifndef EMU_MATH_NOISE_FUNCTORS_H_INC_
+#define EMU_MATH_NOISE_FUNCTORS_H_INC_ 1
+
+#include "_noise_gen_functors/_common_noise_gen_functor_includes.h"
+#include "_noise_gen_functors/NoiseGen1D.h"
+
+#endif
