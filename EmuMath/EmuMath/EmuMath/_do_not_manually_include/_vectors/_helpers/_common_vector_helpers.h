3244b33e8c5affa2e6a37247630a367a413bbe25
BigUglySpider <36843643+BigUglySpider@users.noreply.github.com>
2022-01-26 09:57:39 +0000

    Dead info removed | Sets removed
    Vectors can no longer be `Set` after construction. Originally this was the same as a `Copy` for value-containing Vectors, but reference-containing Vectors would update their references on a call to `Set`.
    
    This behaviour has been removed to follow C++'s focus on non-nullable references. Now references within Vectors will focus on the same memory from birth until death, and no side effects will be modifying that unless an explicit type is used to do so.
       - This also means that dangling references are not modifiable, so now reference Vectors have a clear (and intended) lifetime compared to value Vectors.

--- EmuMath/EmuMath/EmuMath/_do_not_manually_include/_vectors/_helpers/_common_vector_helpers.h
+++ EmuMath/EmuMath/EmuMath/_do_not_manually_include/_vectors/_helpers/_common_vector_helpers.h
@@ -8,7 +8,6 @@
 #include "../_underlying_helpers/_vector_info.h"
 #include "../_underlying_helpers/_vector_misc_arithmetic_underlying.h"
 #include "../_underlying_helpers/_vector_mutation_underlying.h"
-#include "../_underlying_helpers/_vector_set_underlying.h"
 #include "../_underlying_helpers/_vector_special_operations_underlying.h"
 #include "../_underlying_helpers/_vector_stream_append_underlying.h"
 #include "../_underlying_helpers/_vector_tmp.h"
