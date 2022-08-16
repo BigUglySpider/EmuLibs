8f0c03b4ca1d893ec426c3ba36594ff8677c71e4
BigUglySpider <36843643+BigUglySpider@users.noreply.github.com>
2022-08-16 01:42:44 +0100

    Adds Common 3D math includes

---
+++ EmuMath/EmuMath/EmuMath/Common3D.h
@@ -0,0 +1,9 @@
+#ifndef EMU_MATH_COMMON_3D_H_INC_
+#define EMU_MATH_COMMON_3D_H_INC_ 1
+
+#include "Matrix.h"
+#include "Quaternion.h"
+#include "Rect.h"
+#include "Vector.h"
+
+#endif
---
+++ EmuMath/EmuMath/EmuMath/Fast3D.h
@@ -0,0 +1,7 @@
+#ifndef EMU_MATH_FAST_3D_H_INC_
+#define EMU_MATH_FAST_3D_H_INC_ 1
+
+#include "Common3D.h"
+#include "FastVector.h"
+
+#endif
