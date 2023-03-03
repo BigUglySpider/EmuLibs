b9623e40633ec193d78ff7f3684d415319411828
Erica Bellamy <36843643+BigUglySpider@users.noreply.github.com>
2023-03-03 01:22:18 +0000

    Removal of bad trunc call in f64x2

--- EmuMath/EmuMath/EmuSIMD/_do_not_manually_include/_simd_helpers/_template_helpers/_underlying_template_helpers/_generic_funcs/_f64x2.h
+++ EmuMath/EmuMath/EmuSIMD/_do_not_manually_include/_simd_helpers/_template_helpers/_underlying_template_helpers/_generic_funcs/_f64x2.h
@@ -525,8 +525,8 @@ namespace EmuSIMD::Funcs
 
 	EMU_SIMD_COMMON_FUNC_SPEC EmuSIMD::f64x2 mod_f64x2(EmuSIMD::f64x2_arg lhs_, EmuSIMD::f64x2_arg rhs_)
 	{
-		EmuSIMD::f64x2 res = _mm_div_pd(lhs_, rhs_);
-		res = _mm_trunc_pd(res);
+		EmuSIMD::f64x2 res = div_f64x2(lhs_, rhs_);
+		res = trunc_f64x2(res);
 		return _mm_fnmadd_pd(res, rhs_, lhs_);
 	}
 
