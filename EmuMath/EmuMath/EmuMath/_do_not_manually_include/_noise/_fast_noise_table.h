dffa5e6219d5b902f9e4e32deec503a3413ba993
Erica Bellamy <36843643+BigUglySpider@users.noreply.github.com>
2023-04-05 02:15:43 +0100

    .

--- EmuMath/EmuMath/EmuMath/_do_not_manually_include/_noise/_fast_noise_table.h
+++ EmuMath/EmuMath/EmuMath/_do_not_manually_include/_noise/_fast_noise_table.h
@@ -108,11 +108,17 @@ namespace EmuMath
 
 	public:
 #pragma region CONSTRUCTORS
-		FastNoiseTable() noexcept = default;
+		FastNoiseTable() : samples(), table_size()
+		{
+		}
 
-		FastNoiseTable(const this_type& to_copy_) noexcept = default;
+		FastNoiseTable(const this_type& to_copy_) : samples(to_copy_.samples), table_size(to_copy_.table_size)
+		{
+		}
 
-		FastNoiseTable(this_type&& to_move_) noexcept = default;
+		FastNoiseTable(this_type&& to_move_) noexcept : samples(std::move(to_move_.samples)), table_size(std::move(to_move_.table_size))
+		{
+		}
 #pragma endregion
 
 #pragma region RANDOM_ACCESS
@@ -176,8 +182,6 @@ namespace EmuMath
 		{
 			return at(coords_);
 		}
-
-		[[nodiscard]] inline this_type& operator=(const this_type&) noexcept = default;
 #pragma endregion
 
 		[[nodiscard]] inline coordinate_type size() const
